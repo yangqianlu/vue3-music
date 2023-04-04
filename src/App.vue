63f8a42a53a524518f524f68458e22ff54a20b79
杨前露 <yangqianlu@yangqianludeMacBook-Pro.local>
2023-04-04 14:04:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <m-header></m-header>
+</template>
+
+<script>
+import Header from "@/components/header/header";
+export default {
+  name: "app",
+  components: {
+    MHeader: Header,
+  },
+};
+</script>
+<style lang="scss"></style>
